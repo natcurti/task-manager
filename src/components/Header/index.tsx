b0b9781053bef6b39ca0c99f8feb058bbac4f603
natcurti <naticurti01@gmail.com>
2024-08-25 18:39:22 -0300

    Handle edit mode on title and subtitle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,51 @@
+"use client";
+import { useState } from "react";
 import { EditIcon, Logo } from "../Icons";
 import styles from "./Header.module.scss";
 
 const Header = () => {
+  const [editMode, setEditMode] = useState(false);
+  const [boardDetails, setBoardDetails] = useState({
+    title: "My Task Board",
+    subtitle: "Tasks to keep organized",
+  });
+
   return (
     <header className={styles.header}>
       <Logo />
-      <div>
-        <h1 className={styles.title}>My Task Board</h1>
-        <h2 className={styles.subtitle}>Tasks to keep organized</h2>
+      <div className={styles.container}>
+        {editMode ? (
+          <>
+            <input
+              className={styles["input-title"]}
+              value={boardDetails.title}
+              onChange={(e) =>
+                setBoardDetails({
+                  ...boardDetails,
+                  title: e.target.value,
+                })
+              }
+            />
+            <input
+              value={boardDetails.subtitle}
+              onChange={(e) =>
+                setBoardDetails({
+                  ...boardDetails,
+                  subtitle: e.target.value,
+                })
+              }
+            />
+          </>
+        ) : (
+          <>
+            <h1 className={styles.title}>{boardDetails.title}</h1>
+            <h2 className={styles.subtitle}>{boardDetails.subtitle}</h2>
+          </>
+        )}
       </div>
-      <EditIcon />
+      <button className={styles.btn} onClick={() => setEditMode(!editMode)}>
+        <EditIcon />
+      </button>
     </header>
   );
 };
