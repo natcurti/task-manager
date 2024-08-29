077f709b14c18a6e4e24b3462ec2ea86f5911aba
natcurti <naticurti01@gmail.com>
2024-08-28 22:39:17 -0300

    Handle close modal

--- src/components/ModalComponent/index.tsx
+++ src/components/ModalComponent/index.tsx
@@ -1,17 +1,40 @@
+"use client";
+import { useModalContext } from "@/context/modalContext";
 import Button from "../Button";
 import { CloseModal, DoneIcon, TrashIcon } from "../Icons";
 import IconSection from "../IconSection";
 import Input from "../Input";
 import Status from "../Status";
 import styles from "./ModalComponent.module.scss";
+import { useRef } from "react";
 
 const ModalComponent = () => {
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const { isOpen, setIsOpen } = useModalContext();
+
+  const handleClickOverlay = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (e.target === overlayRef.current) {
+      setIsOpen(!isOpen);
+    } else {
+      e.stopPropagation();
+    }
+  };
+
   return (
-    <div className={styles.overlay}>
+    <div
+      className={styles.overlay}
+      ref={overlayRef}
+      onClick={handleClickOverlay}
+    >
       <section className={styles.modal}>
         <div className={styles["container-title"]}>
           <h3 className={styles.title}>Task details</h3>
-          <button className={styles.btnClose}>
+          <button
+            className={styles.btnClose}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <CloseModal />
           </button>
         </div>
